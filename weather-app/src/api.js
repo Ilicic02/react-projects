25550d17e88b72e9f9f700175fbd3c13d81baabe
Anastasija Ilicic <anastasija.ilicic02@gmail.com>
2024-08-31 00:21:02 +0200

    Implement search feature in Weather App

---
+++ weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'cfdfbb16d7msh26ade2e782e2977p174856jsn979528a3c4d1',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
---
+++ weather-app/src/components/search/search.js
@@ -0,0 +1,48 @@
+import {useState} from "react";
+import {AsyncPaginate} from "react-select-async-paginate";
+import {GEO_API_URL, geoApiOptions} from '../../api';
+
+const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    const loadOptions = async (inputValue) => {
+        try {
+            const response = await fetch(
+                `${GEO_API_URL}?minPopulation=1000000&namePrefix=${inputValue}`, 
+                geoApiOptions
+            );
+
+            const result = await response.json();
+
+            return {
+                options: result.data.map((city) => ({
+                    label: `${city.name}, ${city.countryCode}`,
+                    value: city.id
+                }))
+            };
+
+        } catch (error) {
+            console.error(error);
+            return {
+                options: []
+            };
+        }
+    }
+
+    return (
+        <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search} 
+            onChange={handleOnChange}
+            loadOptions={loadOptions}/>
+    )
+}
+
+export default Search;
